fec8bd9a0b17dccc941fdcc54f90b45b1065ad65
Kuat Kurpenov <kurpenov2003@yandex.ru>
2024-08-12 13:28:33 +0600

    Added response structure

---
+++ apd_core/src/response_structure.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct PackagesData {
+    pub request_args: HashMap<String, String>,
+    pub length: u32,
+    pub packages: Vec<Package>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Package {
+    pub name: String,
+    pub epoch: u32,
+    pub version: String,
+    pub release: String,
+    pub arch: String,
+    pub disttag: String,
+    pub buildtime: u32,
+    pub source: String,
+}
